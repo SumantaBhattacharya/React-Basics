1563260b5f024e983affa68e1eb3e4a96bfe221f
SumantaBhattacharya <sumanta2004@gmail.com>
2025-03-27 09:55:19 +0530

    Add additional pages to App component for expanded navigation

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import "./App.css";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
+import Page3 from "./pages/page3";
+import Page4 from "./pages/Page4";
+import Page5 from "./pages/Page5";
+// import Page6 from "./pages/Page6";
 import Header from "./components/Header";
 
 function App() {
@@ -13,6 +17,10 @@ function App() {
       <Header />
       <Page1 />
       <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+      {/* <Page6/> */}
     </div>
     </>
   );
