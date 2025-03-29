8ecc02d37cbb1355f465d2943f9ea4f4e2f28ac5
SumantaBhattacharya <sumanta2004@gmail.com>
2025-03-29 09:16:26 +0530

    Refactor form handling in App component to use a single state object for form data and improve user registration logic

--- Interview-Form_Validation/src/App.jsx
+++ Interview-Form_Validation/src/App.jsx
@@ -3,14 +3,52 @@ import { ToastContainer, toast, Bounce } from "react-toastify";
 
 import "./App.css";
 import "boxicons";
+import Users from "./Components/Users";
 function App() {
   // handle input
+  /*
   const [isName, setName] = useState("");
   const [isEmail, setEmail] = useState("");
   const [isPassword, setPassword] = useState("");
   const [isConfirmPassword, setConfirmPassword] = useState("");
+  */
+
+  const [isFormData, setFormData] = useState({
+    isName: "",
+    isEmail: "",
+    isPassword: "",
+    isConfirmPassword: "",
+  });
 
   const [isError, setError] = useState("");
+
+  const [isUsers, setUsers] = useState([]); // isUsers is an empty array by default
+
+  const handleChanges = (e) => {
+    // destructuring
+    // const {isName, value}
+
+    // console.log(e.target.value);
+    // console.log(e.target.name);
+
+    /*const isName = e.target.name;
+    const value = e.target.value;*/
+
+    // short way for all the inputs
+    const { name, value } = e.target;
+
+    // console.log(name, value);
+
+    /*setFormData({
+      //...isFormData,
+      [name]: value,
+    })*/
+
+    setFormData((prevState) => ({
+      ...prevState, // Spread previous state to keep other fields
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     // camel case
@@ -18,17 +56,17 @@ function App() {
 
     // console.log("Form submitted", { isName, isEmail, isPassword });
 
-    if (isPassword.length < 8) {
+    if (isFormData.isPassword.length < 8) {
       setError("Password must be of 8 characters long!");
       return;
     }
 
-    if (isPassword !== isConfirmPassword) {
+    if (isFormData.isPassword !== isFormData.isConfirmPassword) {
       setError("Password does'nt match with the current password");
       return;
     }
 
-    if (!/[!@#$%^&*()<>,./"']/.test(isPassword)) {
+    if (!/[!@#$%^&*()<>,./"']/.test(isFormData.isPassword)) {
       // if the password does'nt contain any of these symbols that a password contain contain atmost one of them.
       setError(
         "Password must contain at least one of the following symbols: '!@#$%^&*()<>,./' "
@@ -36,25 +74,65 @@ function App() {
       return;
     }
 
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(isEmail)) {
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(isFormData.isEmail)) {
       setError("Invalid email address");
       return;
     } // The ^ ensures that matching starts from the beginning of the email string. Without it, the regex could match a valid email anywhere inside the string, which might allow invalid inputs.
 
     // one atleast capital letter
-    if (!/[A-Z]/.test(isPassword)) {
+    if (!/[A-Z]/.test(isFormData.isPassword)) {
       setError("Password must contain at least one capital letter");
       return;
-    }    
+    }
+
+    // const copyUsers = [...isUser]// spread operator(copy of array
+    // copyUsers.push({ name: isName, email: isEmail });
+
+    // old data + new data
+    // setUsers([...isUsers, { name: isName, email: isEmail }]); // Update the state with new user.
+
+    // setUsers([...isUsers, isFormData]);
+    // setUsers([...isUsers, { name: isFormData.isName, email: isFormData.isEmail }]);
+
+    setUsers((prevUsers) => {
+      return [
+        ...prevUsers,
+        { name: isFormData.isName, email: isFormData.isEmail },
+      ];
+    });
+    // In an arrow function with {}, you must include an explicit return. Otherwise, the function does not return anything
+
+    /*
+    // or
+
+    setUsers((prevUsers) => [
+      ...prevUsers, 
+      { name: isFormData.isName, email: isFormData.isEmail }
+    ]);
+
+
+    */
+
+    // console.log(isUsers);
 
     setError("");
 
+    setFormData({
+      isName: "",
+      isEmail: "",
+      isPassword: "",
+      isConfirmPassword: "",
+    });
+
+    /*
     setName("");
     setEmail("");
     setPassword("");
     setConfirmPassword("");
+    */
 
     toast.success("The User is successfully logged in!", {
+      // This means the toast message appears every time the form submission is successful.
       position: "top-center",
       autoClose: 3004,
       hideProgressBar: false,
@@ -104,56 +182,65 @@ function App() {
               Full Name
             </label>
             <input
-              value={isName}
+              // value={isName}
+              value={isFormData.isName}
               onChange={(e) => {
-                setName(e.target.value);
+                // setName(e.target.value);
+                handleChanges(e);
               }}
               id="name"
+              name="isName"
               type="text"
               placeholder="Enter your name"
               className="p-2 rounded-md  w-full"
               required
             />
-            <label id="email" className="text-white mb-1 underline">
+            <label htmlFor="isEmail" className="text-white mb-1 underline">
               Email
             </label>
             <input
-              value={isEmail}
+              // value={isEmail}
+              value={isFormData.isEmail}
               onChange={(e) => {
-                setEmail(e.target.value);
+                // setEmail(e.target.value);
+                handleChanges(e);
               }}
-              id="email"
+              id="isEmail"
               type="email"
+              name="isEmail"
               placeholder="Enter your email"
               className="p-2 rounded-md  w-full"
               required
             />
-            <label htmlFor="passowrd" className="text-white mb-1 underline">
+            <label htmlFor="isPassword" className="text-white mb-1 underline">
               Password
             </label>
             <input
-              value={isPassword}
+              // value={isPassword}
+              value={isFormData.isPassword}
               onChange={(e) => {
-                setPassword(e.target.value);
+                // setPassword(e.target.value);
+                handleChanges(e);
               }}
-              id="password"
+              id="isPassword"
+              name="isPassword"
               type="password"
               placeholder="Enter your password"
               className="p-2 rounded-md  w-full"
               required
             />
             <label
-              htmlFor="ConfirmPassword"
+              htmlFor="isConfirmPassword"
               className="text-white mb-1 underline"
             >
               Confirm Password
             </label>
             <input
-              value={isConfirmPassword}
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
-              id="ConfirmPassword"
+              // value={isConfirmPassword}
+              value={isFormData.isConfirmPassword}
+              onChange={handleChanges}
+              name="isConfirmPassword"
+              id="isConfirmPassword"
               type="password"
               placeholder="Confirm Password"
               className="p-2 rounded-md w-full"
@@ -190,9 +277,23 @@ function App() {
 
         <ToastContainer></ToastContainer>
       </div>
+      {/* .map() is a method for arrays */}
+
+      <div className="hidden ml-6 sm:flex flex-col gap-4 p-4 bg-gray-900 rounded-lg shadow-lg w-72">
+        <h2 className="text-yellow-400 text-lg font-bold text-center">
+          Registered Users
+        </h2>
+        {isUsers.length === 0 ? (
+          <p className="text-gray-500 text-center">No users yet</p>
+        ) : (
+          isUsers.map((elem, index) => <Users key={index} element={elem} />)
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
 // outline-none
+//  In JSX, the return keyword is unnecessary inside an arrow function when it directly returns JSX.
+// No Need for return in Arrow Function: When using an arrow function {} requires return, but () allows implicit return.
