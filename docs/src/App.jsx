40ad56b120cb19621f3b11ff43b71e7de57518fb
SumantaBhattacharya <sumanta2004@gmail.com>
2025-08-21 10:09:36 +0530

    Add App component with Background and Foreground integration

---
+++ docs/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+import Background from "./components/Background";
+import Foreground from "./components/Foreground";
+
+function App() {
+  return (
+    <div className="App w-full h-screen relative">
+      <Background />
+
+      <Foreground/>
+    </div>
+  );
+}
+
+export default App;
