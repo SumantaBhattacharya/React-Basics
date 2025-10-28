fb5b9e40d90d50bd339437133253f37c2e44dbac
SumantaBhattacharya <sumanta2004@gmail.com>
2025-10-28 10:37:34 +0530

    HeroText

---
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='flex-col justify-between p-6'>
+        <h3 className='text-7xl font-bold leading-[1.1]'>Prospective <br /> <span className='bg-gray-200 rounded-2xl px-3'>customer</span> <br /> segmentation</h3>
+        <p className='text-lg mt-7 font-medium text-gray-800'>Depending on customer satisfaction and access to banking products, potential target audience can be divided into three groups.</p>
+      </div>
+  )
+}
+
+export default HeroText
