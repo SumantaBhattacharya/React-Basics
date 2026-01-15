881e9835bc6cdcd4ee68a179c03dca2d73d7533b
SumantaBhattacharya <sumanta2004@gmail.com>
2026-01-15 11:19:18 +0530

    .js was missing

--- ZERODHA CLONE/backend/src/models/HoldingModel.js
+++ ZERODHA CLONE/backend/src/models/HoldingModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-import holdingSchema from "../schemas/HoldingSchema";
+import holdingSchema from "../schemas/HoldingSchema.js";
 
 // mongoose.model it's a factory function
 const HoldingModel = mongoose.model("Holding", holdingSchema);
