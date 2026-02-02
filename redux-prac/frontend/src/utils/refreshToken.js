f6bb12bf5a36f600e1d9377d72026f08498e91ec
SumantaBhattacharya <sumanta2004@gmail.com>
2026-02-02 09:42:38 +0530

    refreshAccessToken

---
+++ redux-prac/frontend/src/utils/refreshToken.js
@@ -0,0 +1,17 @@
+import axios, { isAxiosError } from "axios";
+export const refreshAccessToken = async () => {
+
+    try{
+        const response = await axios.post("http://localhost:8000/api/auth/refresh-token", {}, {
+            withCredentials: true // crucial for cookies
+        });
+
+        const accessToken = response.data.data.accessToken; // we need the data inside the accesstoken
+        localStorage.setItem("accessToken", accessToken);
+        return accessToken;
+    } catch (err) {
+        console.error("Failed to refresh token:", err.message);
+        throw err;
+    } 
+
+};
