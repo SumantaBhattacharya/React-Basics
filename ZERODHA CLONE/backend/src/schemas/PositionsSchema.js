02865f771a0a200fb307064c81fb541665819dcd
SumantaBhattacharya <sumanta2004@gmail.com>
2026-02-02 09:41:33 +0530

    PositionsSchema

---
+++ ZERODHA CLONE/backend/src/schemas/PositionsSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const PositionsSchema = new mongoose.Schema(
+  {
+    product: String,
+    name: String,
+    qty: Number,
+    avg: Number,
+    price: Number,
+    net: String,
+    day: String,
+    isLoss: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
